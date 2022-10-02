d0fc23265691c733a8d6e5354aa75925a5156ad1
AbdulRehman <abdulrehmanmunaf123@gmail.com>
2022-10-02 09:18:00 -0400

    fix route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,26 +3,15 @@ import * as React from 'react'
 import { Navigate, useRoutes } from 'react-router-dom'
 import { Box } from '@mui/material'
 
-import Login from '../pages/Login/Login'
+import Home from '../pages/Home/Home'
 import NotFound from '../pages/NotFound/NotFound'
 
 export default function Routes(): ReactNode {
     return useRoutes([
-        // {
-        //     path: '/dashboard',
-        //     element: <PrivateRoute navLink="/" component={Dashboard} />,
-        //     children: [
-        //         {
-        //             path: '',
-        //             element: <div />,
-        //         },
-
-        //     ],
-        // },
         { path: '404', element: <NotFound /> },
         {
             path: '/',
-            element: <Login />,
+            element: <Home />,
         },
 
         {
